725b6de92c6e03974ea8eec6e4b5890bb68e6c57
airajaneeeee <ajbassig@gmail.com>
2024-05-24 10:09:04 -0700

    Implement notification when item/request owner accepted someone's interest/offer

--- index.js
+++ index.js
@@ -811,6 +811,46 @@ app.get("/acceptPeopleInterested/:userId/:itemId", async (req, res) => {
     );
   }
 
+
+    
+  //For notification
+  //Find the user in the database
+  const personaccepted = await userCollection.findOne({ _id: new mongodb.ObjectId(userId) });
+
+  //item owner
+  const itemOwner = await userCollection.findOne({ _id: new mongodb.ObjectId(item.user_id) });
+
+  // Find the notification from person accepted's notifications array
+  let notificationIndex = personaccepted.notifications.findIndex(notification => 
+    notification.itemId === itemId);
+  
+  if (notificationIndex !== -1) {
+    // The notification exists, remove it
+    personaccepted.notifications.splice(notificationIndex, 1);
+
+    // Update the user document in the database
+    await userCollection.updateOne({ _id: personaccepted._id }, 
+      { $set: { notifications: personaccepted.notifications } });
+
+  } else {
+    // The notification does not exist, create it
+    let notification = {
+      itemId: itemId,
+      message: `Your interest in ${item.title} has been accepted by ${itemOwner.username}.`,
+      date: new Date()
+    };
+
+  // Add the notification to the person accepted's notifications array
+  personaccepted.notifications.push(notification);
+
+  // Update the person accepted document in the database
+  await userCollection.updateOne({ _id: personaccepted._id }, 
+    { $set: { notifications: personaccepted.notifications } });
+
+  }
+
+
+
   // Redirect back to the item page
   res.redirect("/peopleInterested/" + itemId);
 
@@ -859,6 +899,46 @@ app.get("/acceptPeopleOffering/:userId/:requestId", async (req, res) => {
       { $set: { personaccepted: userId, status: "Pending Exchange" } }
     );
   }
+
+  
+    
+  //For notification
+  //Find the user in the database
+  const personaccepted = await userCollection.findOne({ _id: new mongodb.ObjectId(userId) });
+
+  //item owner
+  const requestOwner = await userCollection.findOne({ _id: new mongodb.ObjectId(request.user_id) });
+
+  // Find the notification from person accepted's notifications array
+  let notificationIndex = personaccepted.notifications.findIndex(notification => 
+    notification.requestId === requestId);
+  
+  if (notificationIndex !== -1) {
+    // The notification exists, remove it
+    personaccepted.notifications.splice(notificationIndex, 1);
+
+    // Update the user document in the database
+    await userCollection.updateOne({ _id: personaccepted._id }, 
+      { $set: { notifications: personaccepted.notifications } });
+
+  } else {
+    // The notification does not exist, create it
+    let notification = {
+      requestId: requestId,
+      message: `Your offer in the ${request.title} has been accepted by ${requestOwner.username}.`,
+      date: new Date()
+    };
+
+  // Add the notification to the person accepted's notifications array
+  personaccepted.notifications.push(notification);
+
+  // Update the person accepted document in the database
+  await userCollection.updateOne({ _id: personaccepted._id }, 
+    { $set: { notifications: personaccepted.notifications } });
+    
+  }
+
+
 
   // Redirect back to the item page
   res.redirect("/peopleOffering/" + requestId);
